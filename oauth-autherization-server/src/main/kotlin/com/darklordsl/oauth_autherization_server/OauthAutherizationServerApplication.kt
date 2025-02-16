c9dcc42a7934cd5fe5599ff4d15ee4bc45472d74
darklordsl <dilshanpgn@gmail.com>
2025-02-16 23:16:53 +0530

    init commit

---
+++ oauth-autherization-server/src/main/kotlin/com/darklordsl/oauth_autherization_server/OauthAutherizationServerApplication.kt
@@ -0,0 +1,11 @@
+package com.darklordsl.oauth_autherization_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OauthAutherizationServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<OauthAutherizationServerApplication>(*args)
+}
---
+++ oauth-autherization-server/src/test/kotlin/com/darklordsl/oauth_autherization_server/OauthAutherizationServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.darklordsl.oauth_autherization_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OauthAutherizationServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
